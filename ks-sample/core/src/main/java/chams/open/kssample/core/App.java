4465b4433763c0f7feb4909c1c2f4f96fcb74eb7
unknown <cam@ES-NB-DEV78.exilesoft.local>
2012-07-17 16:15:37 +0530

    [ks-ps] - init

---
+++ ks-sample/core/src/main/java/chams/open/kssample/core/App.java
@@ -0,0 +1,13 @@
+package chams.open.kssample.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
